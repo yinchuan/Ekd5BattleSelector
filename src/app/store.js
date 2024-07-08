b035f790e314dc9d5436654c3cf510e8b2e48470
yinchuan <yinchuan.blue@gmail.com>
2024-07-08 15:29:17 +1000

    persistent redux

--- src/app/store.js
+++ src/app/store.js
@@ -1,14 +1,29 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
+
 import battleReducer from '../features/battles/battlesSlice'
 import saveIdReducer from '../features/saveId/saveIdSlice'
 import characterReducer from '../features/character/charactersSlice'
 
-const store = configureStore({
-    reducer: {
+const persistConnfig = {
+    key: 'root',
+    storage,
+    stateReconciler: autoMergeLevel2,
+}
+
+const persistedReducer = persistReducer(
+    persistConnfig,
+    combineReducers({
         battles: battleReducer,
         saveId: saveIdReducer,
         characters: characterReducer,
-    },
+    })
+)
+
+const store = configureStore({
+    reducer: persistedReducer,
 })
 
 export default store
--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import React from 'react'
 import './index.css'
 import App from './App'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
 import store from './app/store'
 
 createRoot(document.getElementById('root')).render(
     <Provider store={store}>
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>
+        <PersistGate loading={null} persistor={persistStore(store)}>
+            <React.StrictMode>
+                <App />
+            </React.StrictMode>
+        </PersistGate>
     </Provider>
 )
